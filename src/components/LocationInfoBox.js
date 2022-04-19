7df0f573797d7f104788097886cb33d2ae0f8f12
choisonghee <songhee0215@lgupluspartners.co.kr>
2022-04-19 15:52:17 +0900

    location marker 추가

--- src/components/LocationInfoBox.js
+++ src/components/LocationInfoBox.js
@@ -1,9 +1,10 @@
-import React from 'react';
-
-const LocationInfoBox = () => {
+const LocationInfoBox = ({ info }) => {
   return (
-    <div>
-      
+    <div className="location-info">
+      <h2 className="location-info__title">Event Location Info</h2>
+      <ul className="location-info__group">
+        <li className="location-info__list"></li>
+      </ul>
     </div>
   );
 };
